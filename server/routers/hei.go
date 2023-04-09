cc8a864f945200e09b4ce557fc917ef475efd1d9
lavenderttttttt <3083145959@qq.com>
2023-04-09 07:06:36 -0900

    添加FilterHEI函数

--- server/routers/hei.go
+++ server/routers/hei.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"melodie-site/server/services"
 	"net/http"
-
+	"strconv"
+	"melodie-site/server/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,5 +33,48 @@ func GetHEIByName(c *gin.Context) {
 // url参数名称参照services.GetHEIService().FilterHEI()的输入参数
 // 要求自测通过，对于异常输入，返回中文的报错提示。
 func FilterHEI(c *gin.Context) {
-	// services.GetHEIService().FilterHEI()
+	//拿到参数provincialLocation
+	provincialLocation := c.Query("provincialLocation")
+
+	//level和mode这两个参数要做类型转换，不知道学长有没有更简便的方法（我这里写的感觉笨笨的
+	s := c.Query("level")
+	var levels models.HEILevel
+	if s != ""{
+		level, err := strconv.Atoi(s)
+		//如果输入level不为数字，就出错
+		if err !=nil{
+			c.JSON(http.StatusNotFound, makeResponse(false, errors.New(fmt.Sprintf("level参数格式错误")), nil))
+			return
+		}
+		levels =models.HEILevel(level)
+	}else{
+		levels=-1
+	}
+	//参数mode
+	p := c.Query("mode")
+	var modes models.HEIMode
+	if p!=""{
+		mode, err := strconv.Atoi(p)
+		//如果输入mode不为数字，就出错
+		if err !=nil{
+			c.JSON(http.StatusNotFound, makeResponse(false, errors.New(fmt.Sprintf("mode参数格式错误")), nil))
+			return
+		}
+		modes =models.HEIMode(mode)
+	}else{
+		modes=-1
+	}
+    //参数policy
+	policy := c.Query("policy")
+
+	
+	heis, err := services.GetHEIService().FilterHEI(provincialLocation, levels, modes, policy)
+	//这里不太懂：如果service方法err不为空的话，都是什么情况？
+	//(我这里测试：如果数据库中不存在相关数据(比如我设置level=3，这里没有符合条件的学校)，
+	//services包里面FilterHEI方法中Find方法的返回值err为空，但是学长您写的GetHEIByName方法中如果没有对应学校会返回的err不为nil)
+	if err !=nil{
+		c.JSON(http.StatusNotFound, makeResponse(false, errors.New(fmt.Sprintf("数据库查询错误")), nil))
+	}else{
+		c.JSON(http.StatusOK, makeResponse(true, nil, heis))
+	}
 }
