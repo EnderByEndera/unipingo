0d3269affa7e1dc2db5713b17e91980ece2d2569
EnderByEndera <enderbybear@foxmail.com>
2023-04-25 07:08:19 +0800

    1. server.go中添加了orderRouter 2. config.go中添加了获取项目根目录的方法 3. 添加了几个订单API测试

--- server/config/config.go
+++ server/config/config.go
@@ -3,9 +3,18 @@ package config
 import (
 	"io/ioutil"
 	"log"
+	"path"
+	"runtime"
 
 	"gopkg.in/yaml.v2"
 )
+
+func GetRootFilepath() string {
+	_, filename, _, _ := runtime.Caller(0)
+	// TODO：先暂时这样获取项目根目录，之后有更好的办法再说
+	// 		 一旦项目整体路径发生更改该方法就有可能失效
+	return path.Dir(path.Dir(path.Dir(filename)))
+}
 
 type Config struct {
 	INFRASTRUCTURE_USER struct {
@@ -32,9 +41,10 @@ var config *Config = nil
 
 func ReadConfig() *Config {
 	var f []byte
-	f, err := ioutil.ReadFile("config.yaml")
+	rootPath := GetRootFilepath()
+	f, err := ioutil.ReadFile(path.Join(rootPath, "config.yaml"))
 	if err != nil {
-		f, err = ioutil.ReadFile("config/config.yaml")
+		f, err = ioutil.ReadFile(path.Join(rootPath, "config/config.yaml"))
 		if err != nil {
 			log.Fatalln("no configure file 'config.yaml' or 'config/config.yaml' " + err.Error())
 		}
